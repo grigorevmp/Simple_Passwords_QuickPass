cd12d8b0099c987d2a8d4f55949b3dd59fcb276a
Mikhail <16112000m@gmail.com>
2020-06-01 20:05:10 +0300

    - password generator (alpha) - length setting

--- app/src/main/java/com/mikhailgrigorev/quickpass/PassGenActivity.kt
+++ app/src/main/java/com/mikhailgrigorev/quickpass/PassGenActivity.kt
@@ -6,16 +6,13 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.database.Cursor
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.MotionEvent
-import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.children
-import androidx.core.view.marginTop
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.activity_pass_gen.*
 
@@ -30,7 +27,9 @@ class PassGenActivity : AppCompatActivity() {
     private var useLetters = false
     private var useNums = false
 
-    @SuppressLint("Recycle", "ClickableViewAccessibility", "ResourceAsColor")
+    @SuppressLint("Recycle", "ClickableViewAccessibility", "ResourceAsColor", "RestrictedApi",
+        "SetTextI18n"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pass_gen)
@@ -69,7 +68,7 @@ class PassGenActivity : AppCompatActivity() {
             commit()
         }
 
-        viewAccount.setOnClickListener {
+        userAvatar.setOnClickListener {
             val intent = Intent(this, AccountActivity::class.java)
             intent.putExtra("login", login)
             startActivity(intent)
@@ -110,7 +109,24 @@ class PassGenActivity : AppCompatActivity() {
             }
         }
 
+        lengthToggle.text = getString(R.string.length)  + ": " +  length
         lengthToggle.setOnClickListener {
+            val txt = EditText(this)
+            txt.hint = "$length"
+            AlertDialog.Builder(this)
+                .setTitle("Length of the password")
+                .setMessage("Input length of your password")
+                .setView(txt,  20, 0, 20, 0)
+                .setPositiveButton(
+                    "Set"
+                ) { _, _ ->
+                    length = txt.text.toString().toInt()
+                }
+                .setNegativeButton(
+                    "Cancel"
+                ) { _, _ -> }
+                .show()
+            lengthToggle.text = getString(R.string.length)  + ": " + length
             toast("Length is $length")
         }
 
@@ -149,14 +165,6 @@ class PassGenActivity : AppCompatActivity() {
                 }
                 false
             }
-
-
-        copyPass.setOnClickListener {
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("Password", genPasswordIdField.text.toString())
-            clipboard.setPrimaryClip(clip)
-            toast(getString(R.string.passCopied))
-        }
 
         genPasswordId.setOnClickListener {
             if(genPasswordIdField.text.toString() != ""){
